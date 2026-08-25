706963098b20a96dfbb3e5fc50959adccb1dc1fc
Proteuzin <elielfrank640@gmail.com>
2026-08-25 07:48:00 -0300

    comentario

--- Js/telalogin_cadastro.js
+++ Js/telalogin_cadastro.js
@@ -1,3 +1,4 @@
+/* Função que oculta e mostra a senha ao usuário */
 function mostrarSenha(id, botao) {
     const senha = document.getElementById(id);
     const imagem = botao.querySelector("img");
